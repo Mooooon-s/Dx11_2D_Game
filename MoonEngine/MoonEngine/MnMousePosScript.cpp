6a8e2ab5a7c04f8488280cc78df853b98576507c
Mooooon-s <mmk990212@naver.com>
2023-08-24 17:58:09 +0900

    Click primary Object

--- MoonEngine/MoonEngine/MnMousePosScript.cpp
+++ MoonEngine/MoonEngine/MnMousePosScript.cpp
@@ -15,34 +15,70 @@ namespace Mn
 	MousePosScript::MousePosScript()
 		: _Cam(nullptr)
 		, _Idx(1)
+		, _GameObjects{}
 	{
+		_Setting.reset();
+		_Setting.set((UINT)eLayerType::Coin, 1);
+		_Setting.set((UINT)eLayerType::Pet, 1);
+		_Setting.set((UINT)eLayerType::Monster, 1);
 	}
 	MousePosScript::MousePosScript(GameObject* cam)
-		:_Cam(cam)
+		: _Cam(cam)
+		, _Idx(1)
+		, _GameObjects{}
 	{
+		_Setting.reset();
+		_Setting.set((UINT)eLayerType::Coin, 1);
+		_Setting.set((UINT)eLayerType::Pet, 1);
+		_Setting.set((UINT)eLayerType::Monster, 1);
 	}
 	MousePosScript::~MousePosScript()
 	{
 	}
 	void MousePosScript::Initialize()
 	{
+
 	}
 	void MousePosScript::Update()
 	{
-		if (Input::GetKeyDown(eKeyCode::LBUTTON) 
-			&& _UnProjectPos.x > -2.0f && _UnProjectPos.x < 2.0f 
-			&& _UnProjectPos.y > -1.5f && _UnProjectPos.y < 1.0f )
+		if (Input::GetKeyDown(eKeyCode::LBUTTON))
 		{
-			float a = (0.0001 * _Idx)+1;
-			Vector3 pos = Vector3(_UnProjectPos.x, _UnProjectPos.y, a);
-			Food* food = object::Instantiate<Food>(pos,eLayerType::Food);
-			food->Initialize();
-			_Idx+=1;
-			if (_Idx >= 100)
-				_Idx = 1;
+			_DotPos = _UnProjectPos;
+			GatherObject();
+
+			for (auto Obj : _GameObjects)
+			{
+				if (Obj->GetComponent<Collider2D>() != nullptr)
+					InterSect(Obj->GetComponent<Collider2D>());
+			}
+
+			GameObject* Obj = PrimaryObj();
+
+			if (Obj != nullptr)
+				Obj->Click();
+			else
+				FeedFood();
 		}
 	}
 	void MousePosScript::LateUpdate()
+	{
+		
+		_UnProjectPos = CalculatePos();
+
+		HWND hwnd = application.GetHwnd();
+		wchar_t szPos[50] = {};
+
+		//프레임 계산
+		swprintf_s(szPos, 50, L"x : %lf, y : %lf", _UnProjectPos.x, _UnProjectPos.y);
+		SetWindowText(hwnd, szPos);
+
+		_GameObjects.clear();
+		_InterSectObjs.clear();
+	}
+	void MousePosScript::Render()
+	{
+	}
+	Vector3 MousePosScript::CalculatePos()
 	{
 		Transform* tr = _Cam->GetComponent<Transform>();
 		Matrix world = tr->WorldMatrix();
@@ -58,16 +94,73 @@ namespace Mn
 		viewport.y = 0;
 		viewport.minDepth = 0.0f;
 		viewport.maxDepth = 1.0f;
-		_UnProjectPos = viewport.Unproject(_WinPos, Camera::GetGpuProjectionMatrix(), Camera::GetGpuViewMatrix(), Matrix::Identity);
 
-		HWND hwnd = application.GetHwnd();
-		wchar_t szPos[50] = {};
+		return viewport.Unproject(_WinPos, Camera::GetGpuProjectionMatrix(), Camera::GetGpuViewMatrix(), Matrix::Identity);
 
-		//프레임 계산
-		swprintf_s(szPos, 50, L"x : %lf, y : %lf", _UnProjectPos.x, _UnProjectPos.y);
-		SetWindowText(hwnd, szPos);
 	}
-	void MousePosScript::Render()
+	void MousePosScript::InterSect(Collider2D* coll)
 	{
+		Vector3 pos = coll->GetPosition();
+		Vector3 height = coll->HeightVector();
+		Vector3 width =  coll->WidthVector();
+
+		Vector3 Max = pos + height + width;
+		Vector3 Min = pos - height - width;
+
+		if (_DotPos.x >= Min.x && _DotPos.x <= Max.x
+			&& _DotPos.y >= Min.y && _DotPos.y <= Max.y)
+		{
+			_InterSectObjs.push_back(coll->GetOwner());
+		}
+	}
+	void MousePosScript::GatherObject()
+	{
+		Scene* scene = SceneManager::ActiveScene();
+		for (int i = 0; i < (UINT)eLayerType::End; i++)
+		{
+			if (_Setting[i] == true)
+			{
+				std::vector<GameObject*> LayerObj = scene->GetLayer(eLayerType(i)).GetGameObjects();
+				for (auto Obj : LayerObj)
+				{
+					_GameObjects.push_back(Obj);
+				}
+			}
+		}
+	}
+	GameObject* MousePosScript::PrimaryObj()
+	{
+		if(_InterSectObjs.size()==0)
+			return nullptr;
+
+		GameObject* primaryObj = nullptr;
+
+		for (auto obj : _InterSectObjs)
+		{
+			if (primaryObj == nullptr)
+				primaryObj = obj;
+			
+			Vector3 ObjPos = obj->GetComponent<Transform>()->Position();
+			Vector3 PriPos = primaryObj->GetComponent<Transform>()->Position();
+
+			if (ObjPos.z < PriPos.z)
+				primaryObj = obj;
+		}
+
+		return primaryObj;
+	}
+	void MousePosScript::FeedFood()
+	{
+		if (_UnProjectPos.x > -2.0f && _UnProjectPos.x < 2.0f
+			&& _UnProjectPos.y > -1.5f && _UnProjectPos.y < 1.0f)
+		{
+			float a = (0.0001 * _Idx) + 1;
+			Vector3 pos = Vector3(_UnProjectPos.x, _UnProjectPos.y, a);
+			Food* food = object::Instantiate<Food>(pos, eLayerType::Food);
+			food->Initialize();
+			_Idx += 1;
+			if (_Idx >= 100)
+				_Idx = 1;
+		}
 	}
 }
--- MoonEngine/MoonEngine/MnMousePosScript.h
+++ MoonEngine/MoonEngine/MnMousePosScript.h
@@ -4,6 +4,8 @@
 
 namespace Mn
 {
+	#define LAYERMAX (UINT)eLayerType::End
+
 	class MousePosScript : public Script
 	{
 	public:
@@ -17,11 +19,21 @@ namespace Mn
 		void LateUpdate() override;
 		void Render() override;
 	public:
+		Vector3 CalculatePos();
+		void InterSect(Collider2D* coll);
+		void GatherObject();
+		GameObject* PrimaryObj();
+		void FeedFood();
+	public:
 		void UICamera(GameObject* uicamera) { _Cam = uicamera; }
 	private:
 		UINT		_Idx;
+		Vector3		_DotPos;
 		Vector3		_WinPos;
 		Vector3		_UnProjectPos;
 		GameObject* _Cam;
+		std::vector<GameObject*> _GameObjects;
+		std::vector<GameObject*> _InterSectObjs;
+		std::bitset<LAYERMAX> _Setting;
 	};
 }
